8267dcfdfd60326a04b768c9ebcf8e5280f0d155
Shubham Dhopat <shubhamdhopat@Shubhams-MacBook-Air.local>
2026-06-07 23:10:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Country } from "./pages/Country";
+import { Contact } from "./pages/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+
+  {
+    path: "/about",
+    element: <About />,
+  },
+
+  {
+    path: "/country",
+    element: <Country />,
+  },
+
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
