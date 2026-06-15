da3fd21fd5891653789f418353aae7c8092fe458
Shubham Dhopat <shubhamdhopat@Shubhams-MacBook-Air.local>
2026-06-16 00:02:11 +0530

    added contact & footers section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,49 @@
 import React from "react";
 
 export const Contact = () => {
-  return <div>Contact Page</div>;
+  const handleFormSubmit = (formData) => {
+    // console.log(formData.entries());
+
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData);
+  };
+  return (
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+
+      <div className="contact-wrapper container">
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            required
+            autoComplete="off"
+            placeholder="Enter your name"
+            name="username"
+          ></input>
+
+          <input
+            type="email"
+            className="form-control"
+            required
+            autoComplete="off"
+            placeholder="Enter your email"
+            name="email"
+          ></input>
+
+          <textarea
+            name="message"
+            className="form-control"
+            rows="10"
+            placeholder="Enter your message"
+            required
+            autoComplete="off"
+          ></textarea>
+
+          <button type="submit" value="Send">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
